3ac68bc55065612565d9df9f11641c1007cfa35c
Andrew Muse <105908498+Amuse93@users.noreply.github.com>
2024-09-09 22:51:41 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,29 +13,29 @@ function App() {
     });
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  function goToLinkedIn() {
+    window.location.href = "https://www.linkedin.com/in/andrew-muse/";
   }
+
+  function goToFacebook() {
+    window.location.href = "https://www.facebook.com/andrew.muse.9";
+  }
+
+  function goToLinkedIn() {
+    window.location.href = "https://github.com/Amuse93";
+  }
+
+  function 
 
   return (
     <main>
       <h1>Andrew Muse</h1>
-      <a href="https://www.linkedin.com/in/andrew-muse/" target="_blank" rel="noopener noreferrer">
-        Connect with me on LinkedIn
-      </a>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <br />
+      <p>Experienced IT Support Specialist with a strong track record in managing IT operations and driving user satisfaction. Proven ability to develop and implement standard operating procedures to streamline incident resolution, along with delivering in-depth training to both technical teams and end-users. Skilled in logistical coordination, including network configurations and relocations, ensuring smooth business continuity. Proficient in Active Directory management, incident ticket tracking, and hands-on troubleshooting of hardware and networking issues. Adept in Python, Java, web development, and Windows Server administration, bringing a combination of technical expertise and customer-focused solutions to enhance IT support services.</p>
+      <br />
+      <button onClick={goToLinkedIn}>LinkedIn</button>
+      <button onClick={goToFacebook}>Facebook</button>
+      <button onClick={goToGitHub}>GitHub</button>
     </main>
   );
 }
