39d677291432e9609bfc31b8f6bc0f7e9749fdae
Andrew Muse <105908498+Amuse93@users.noreply.github.com>
2024-09-09 23:03:15 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,13 +25,19 @@ function App() {
     window.location.href = "https://github.com/Amuse93";
   }
 
-  function 
-
   return (
     <main>
       <h1>Andrew Muse</h1>
       <br />
-      <p>Experienced IT Support Specialist with a strong track record in managing IT operations and driving user satisfaction. Proven ability to develop and implement standard operating procedures to streamline incident resolution, along with delivering in-depth training to both technical teams and end-users. Skilled in logistical coordination, including network configurations and relocations, ensuring smooth business continuity. Proficient in Active Directory management, incident ticket tracking, and hands-on troubleshooting of hardware and networking issues. Adept in Python, Java, web development, and Windows Server administration, bringing a combination of technical expertise and customer-focused solutions to enhance IT support services.</p>
+      <p>
+        Experienced IT Support Specialist with a strong track record in managing IT operations and driving user satisfaction. 
+        Proven ability to develop and implement standard operating procedures to streamline incident resolution, 
+        along with delivering in-depth training to both technical teams and end-users. Skilled in logistical coordination, 
+        including network configurations and relocations, ensuring smooth business continuity. Proficient in Active Directory management, 
+        incident ticket tracking, and hands-on troubleshooting of hardware and networking issues. Adept in Python, Java, 
+        web development, and Windows Server administration, bringing a combination of technical expertise and customer-focused solutions 
+        to enhance IT support services.
+      </p>
       <br />
       <button onClick={goToLinkedIn}>LinkedIn</button>
       <button onClick={goToFacebook}>Facebook</button>
