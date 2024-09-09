f11e03081682b3e43acf97f292ee6eb870ea876a
Andrew Muse <105908498+Amuse93@users.noreply.github.com>
2024-09-10 00:27:04 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 //import { useEffect, useState } from "react";
 //import type { Schema } from "../amplify/data/resource";
 //import { generateClient } from "aws-amplify/data";
+import image from './assets/andrew_muse_photo.jpg'; // Adjust the path according to your file structure
+
 
 //const client = generateClient<Schema>();
 
@@ -27,17 +29,20 @@ function App() {
 
   return (
     <main>
-      <h1>Andrew Muse</h1>
-      <br />
-      <p>
-        Experienced IT Support Specialist with a strong track record in managing IT operations and driving user satisfaction. 
-        Proven ability to develop and implement standard operating procedures to streamline incident resolution, 
-        along with delivering in-depth training to both technical teams and end-users. Skilled in logistical coordination, 
-        including network configurations and relocations, ensuring smooth business continuity. Proficient in Active Directory management, 
-        incident ticket tracking, and hands-on troubleshooting of hardware and networking issues. Adept in Python, Java, 
-        web development, and Windows Server administration, bringing a combination of technical expertise and customer-focused solutions 
-        to enhance IT support services.
-      </p>
+      <div className="bordered-container">
+        <img src={image} />
+        <h1>Andrew Muse</h1>
+        <br />
+        <p>
+          Experienced IT Support Specialist with a strong track record in managing IT operations and driving user satisfaction. 
+          Proven ability to develop and implement standard operating procedures to streamline incident resolution, 
+          along with delivering in-depth training to both technical teams and end-users. Skilled in logistical coordination, 
+          including network configurations and relocations, ensuring smooth business continuity. Proficient in Active Directory management, 
+          incident ticket tracking, and hands-on troubleshooting of hardware and networking issues. Adept in Python, Java, 
+          web development, and Windows Server administration, bringing a combination of technical expertise and customer-focused solutions 
+          to enhance IT support services.
+        </p>
+      </div>
       <br />
       <button onClick={goToLinkedIn}>LinkedIn</button>
       <button onClick={goToFacebook}>Facebook</button>
